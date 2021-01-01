902876026b0817abd28b26ca7196045ccf80b617
kevin-grylls <eilgwon88@gmail.com>
2021-01-01 10:47:22 +0900

    Updates

---
+++ precache-manifest.2307117c3aa8c3e81062020f4c5299a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ea0d5c348870c610a0f47240eac347",
+    "url": "/coin-chart/index.html"
+  },
+  {
+    "revision": "50ba60baee0ac506497b",
+    "url": "/coin-chart/static/js/2.c7da2e6f.chunk.js"
+  },
+  {
+    "revision": "0c5745de710aa36b50ad4a55e3cb992d",
+    "url": "/coin-chart/static/js/2.c7da2e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9d02083d33248ed648",
+    "url": "/coin-chart/static/js/3.5d0b953e.chunk.js"
+  },
+  {
+    "revision": "6e80509c2d12d02c9d06",
+    "url": "/coin-chart/static/js/4.d5736060.chunk.js"
+  },
+  {
+    "revision": "d288988d3fb299c15c72",
+    "url": "/coin-chart/static/js/5.49e3dbef.chunk.js"
+  },
+  {
+    "revision": "e4157e933dbe8ad031c5",
+    "url": "/coin-chart/static/js/main.183ea39f.chunk.js"
+  },
+  {
+    "revision": "df04c5fa1a2ca1c82cc6",
+    "url": "/coin-chart/static/js/runtime-main.a7909f82.js"
+  }
+]);
